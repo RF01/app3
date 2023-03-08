ce34dcaadb8df472dda43055e1c304bbddaa2a04
RufAvi <javila@birt.eus>
2023-03-08 21:40:31 +0100

    creo compot formulario

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,12 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { FormularioBasicoComponent } from './formulario-basico/formulario-basico.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    FormularioBasicoComponent
   ],
   imports: [
     BrowserModule,
---
+++ src/app/formulario-basico/formulario-basico.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-formulario-basico',
+  templateUrl: './formulario-basico.component.html',
+  styleUrls: ['./formulario-basico.component.css']
+})
+export class FormularioBasicoComponent {
+
+}
